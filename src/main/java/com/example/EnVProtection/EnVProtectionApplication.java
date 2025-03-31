cab35507c1b4fccc2f6e46319d50d2bf35f38417
WokCao <henryquoccao@gmail.com>
2025-03-31 09:18:45 +0700

    Init source code

---
+++ src/main/java/com/example/EnVProtection/EnVProtectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.EnVProtection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnVProtectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnVProtectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EnVProtection/EnVProtectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EnVProtection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnVProtectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
